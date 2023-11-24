061246c04aea75b11714c5f6e861fd91fcf1d0dd
G0oCe <alim.sadigov20@gmail.com>
2023-11-24 23:16:08 +0400

    Add template and function

--- container.cpp
+++ container.cpp
@@ -1,6 +1,36 @@
 #include <iostream>
+#include <vector>
+#include <list>
+#include <deque>
+#include <array>
+
+
+template <typename T>
+void print(const T& container, char delimiter);
 
 int main() {
-
-	return 0;
+    std::list<int> l = {2, 4, 6, 8};
+    std::deque<int> d = {2, 4, 6, 8};
+    std::vector<int> v = {2, 4, 6, 8};
+    std::array<int, 4> arr = {2, 4, 6, 8};
+  
+    print(arr, ',');
+    
+    return 0;
 }
+
+template <typename T>
+void print(const T& container, char delimiter) {
+    auto iter = container.begin();
+
+    while (iter != container.end()) {
+        std::cout << *iter;
+        ++iter;
+
+        if (iter != container.end()) {
+            std::cout << delimiter;
+        }
+    }
+}
+
+	
